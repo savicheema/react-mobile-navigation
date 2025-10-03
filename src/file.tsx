226a52db4a3e70a04d20793dfbb520a83f0789f0
Savitoj Cheema <savitoj.cheema@gmail.com>
2025-10-03 19:03:36 +0530

    setup

---
+++ src/file.tsx
@@ -0,0 +1,1 @@
+export const ChildComponent = () => <>ChildComponent 9</>;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { ChildComponent } from "./file.tsx";
+
+const MyComponent = () => {
+  return (
+    <div>
+      MyComponent 11
+      <ChildComponent />
+    </div>
+  );
+};
+
+export default MyComponent;
