a09c37313c0148c71087035e9fe3b528a48b304e
Savitoj Cheema <savitoj.cheema@gmail.com>
2025-10-05 16:26:34 +0530

    test(inital tests): init pass

--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -2,14 +2,30 @@ import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import type { RenderResult } from "vitest-browser-react";
 import { render } from "vitest-browser-react";
 
-import MyComponent from "../src/index.tsx";
+import MobileNavigation from "../src/index.tsx";
 
-describe("My Component", () => {
-  it("should have MyComponent text", () => {
-    const screen: RenderResult = render(<MyComponent />);
+describe("MobileNavigation", () => {
+  it("should have MobileNavigation in document", () => {
+    const screen: RenderResult = render(<MobileNavigation contents={[]} />);
 
-    const myComponent = screen.getByTestId("my-component");
+    const mobileNavigation = screen.getByTestId("mobile-navigation");
 
-    expect(myComponent).toHaveTextContent("MyComponent");
+    expect(mobileNavigation).toBeInTheDocument();
+  });
+
+  it("should have NavigationBar in document", () => {
+    const screen: RenderResult = render(<MobileNavigation contents={[]} />);
+
+    const navigationBar = screen.getByTestId("navigation-bar");
+
+    expect(navigationBar).toBeInTheDocument();
+  });
+
+  it("should have ContentContainer in document", () => {
+    const screen: RenderResult = render(<MobileNavigation contents={[]} />);
+
+    const contentContainer = screen.getByTestId("content-container");
+
+    expect(contentContainer).toBeInTheDocument();
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        globals: true,
-        coverage: {
-            reporter: ['text', 'html']
-        },
-        browser: {
-            name: "chromium",
-            enabled: true,
-            provider: "playwright",
-            instances: [
-                { browser: "chromium" }
-            ]
-        }
-    }
-})
+  test: {
+    globals: true,
+    coverage: {
+      reporter: ["text", "html"],
+    },
+    browser: {
+      name: "chromium",
+      enabled: true,
+      provider: "playwright",
+      headless: true,
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
